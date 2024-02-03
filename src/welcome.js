cddbab00060cc963c0d01da3646df4227d9b71b3
Esha Sidhu <esidhu21@gmail.com>
2024-02-02 23:18:29 -0800

    creates welcome page
    -creates welcome.js and starts working on bare bones framework for the page

--- src/welcome.js
+++ src/welcome.js
@@ -1,22 +1,15 @@
-function App() {
+function Welcome() {
     return (
       <div className="App">
+        <div>
+            <button>Sign Up</button>
+            <button>Log In</button>
+        </div>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <h1> Welcome! </h1>
         </header>
       </div>
     );
   }
   
-  export default App;
+  export default Welcome;
