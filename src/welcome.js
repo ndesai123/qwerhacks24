eef511bc0fbbc1d605d02a16dc9ec50486ff74d0
ShenranChen <shenran0829@gmail.com>
2024-02-03 01:29:52 -0800

    trying out google sign in

--- src/welcome.js
+++ src/welcome.js
@@ -1,9 +1,35 @@
+import { auth, googleProvider } from './firebase.js';
+import { signInWithPopup } from 'firebase/auth';
+import { Button } from '@mui/material';
+
 function Welcome() {
+
+  function googleSignIn(){
+    try {
+      signInWithPopup(auth, googleProvider);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+//   {
+//     user ? (
+//       <button>
+//   Profile
+// </button>
+
+//     ) : (
+//       <Button variant='outlined' onClick={googleSignIn}>
+//       Join!
+//     </Button>
+
+//     )}
     return (
       <div className="App">
         <div>
-            <button>Sign Up</button>
-            <button>Log In</button>
+          <Button variant='outlined' onClick={googleSignIn}>
+              Join!
+          </Button>
         </div>
         <header className="App-header">
             <h1> Welcome! </h1>
