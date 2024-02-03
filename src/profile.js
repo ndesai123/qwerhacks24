e0c44c11619f9674092dd1de95dc5ed27f3a00ac
Natalie Huang <yingyan.huangg@gmail.com>
2024-02-03 11:31:00 -0800

    profile page updates

--- src/profile.js
+++ src/profile.js
@@ -18,12 +18,21 @@ function Profile(){
             <div class = "flex-container">
                 <div class = "profilebox">
                     <div class = "profileboxtext">My Events</div>
+                    <hr class="line"></hr>
+                    <div class = "eventitem">Event 1</div>
+                    <div class = "eventitem">Event 1</div>
+                    <div class = "eventitem">Event 1</div>
                     <button type = "button" class = "profilebutton">
                         <label class = "buttontext">Create Event</label>
                     </button>
+                    
                 </div>
                 <div class = "profilebox">
                     <div class = "profileboxtext">Interested Events</div>
+                    <hr class = "line"></hr>
+                    <div class = "eventitem">Event 1</div>
+                    <div class = "eventitem">Event 1</div>
+                    <div class = "eventitem">Event 1</div>
                     <button class = "profilebutton">
                         <label class = "buttontext">Explore Events</label>
                     </button>
