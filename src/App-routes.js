9d93481a22c48f44ab2fda5e305039fe1e153188
Esha Sidhu <esidhu21@gmail.com>
2024-02-03 16:26:10 -0800

    creates working buttons
    - switches to different pages from dropdown of profile button

--- src/App-routes.js
+++ src/App-routes.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Welcome from './components/welcome'; // Import your components
 import FeedPage from './feed';
-
+import Profile from './profile';
+import EventPage from './components/createEvent';
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Welcome/>} />
-        <Route path="/feed/" element={<FeedPage/>} />
-        {/* <Route path="/account/" exact component={} />
-        <Route path="/create-event/" exact component={} /> */}
+        <Route path="/feed" element={<FeedPage/>} />
+        <Route path="/account" element={<Profile/>} />
+        <Route path="/create-event" element={<EventPage/>} />
         
       </Routes>
     </Router>
